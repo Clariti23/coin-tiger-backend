d32d0770ae3b2162110a249ba19cc8ed7127f904
Farmwell <g.ferowich@gmail.com>
2020-01-30 12:13:01 -0500

    wrote controller actions for users, favorites, baskets

--- coin-tiger-backend/app/controllers/favorites_controller.rb
+++ coin-tiger-backend/app/controllers/favorites_controller.rb
@@ -1,2 +1,27 @@
 class FavoritesController < ApplicationController
+
+    def index 
+        favorites = Favorite.all 
+        render json: favorites
+    end
+
+    def show 
+        favorite = Favorite.find(params[:id])
+        render json: note
+    end
+
+    def create 
+        favorite = Favorte.create(favorite_params)
+    end
+
+    def destroy
+        favorite = Favorites.find(params[:id])
+        favorite.destroy
+    end
+
+    private 
+
+    def favorite_params
+        params.require(:favorite).permit(:apiId, :name, :symbol, :user_id)
+    end
 end
