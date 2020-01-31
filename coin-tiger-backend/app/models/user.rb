5a7e9f4ec0e0e6bd25159142961b8d64502bec9b
Farmwell <g.ferowich@gmail.com>
2020-01-31 14:05:34 -0500

    generated user serializer, updated routes for users

--- coin-tiger-backend/app/models/user.rb
+++ coin-tiger-backend/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :favorites
     has_many :baskets
+    has_secure_password
+
+    
 end
--- coin-tiger-backend/config/routes.rb
+++ coin-tiger-backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :baskets
   resources :favorites
-  resources :users
+  resources :users, only: [:create, :index]
+
+  post '/login', to: "auth#create"
+  get '/profile', to: "users#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
