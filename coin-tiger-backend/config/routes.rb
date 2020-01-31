45ab7b02d127bc723ad01d682fc67215652214c5
Farmwell <g.ferowich@gmail.com>
2020-01-31 14:32:47 -0500

    verified functionality of user create action

--- coin-tiger-backend/config/routes.rb
+++ coin-tiger-backend/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :favorites
   resources :users, only: [:create, :index]
 
-  post '/login', to: "auth#create"
-  get '/profile', to: "users#profile"
+    post '/login', to: "auth#create"
+    get '/profile', to: "users#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
