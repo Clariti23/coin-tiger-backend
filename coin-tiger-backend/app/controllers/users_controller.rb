4cba95c0efae32736231d5358175f082ec9bbe22
Farmwell <g.ferowich@gmail.com>
2020-01-31 13:18:03 -0500

    logic for users, fav, baskets controllers

--- coin-tiger-backend/app/controllers/users_controller.rb
+++ coin-tiger-backend/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+    def index 
+        users = User.all 
+        render json: users
+    end
+
+    def show 
+        @user = User.find(params[:id])
+        if @user 
+            render json: {
+                user: @user
+            }
+        else
+            render json: {
+                status: 500,
+                errors: ['user not found']
+            }    
+        end
+    end    
+
+    def create
+        @user = User.create(user_params)
+
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: {user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+            render json: { error: 'failed to create user '}, status: :not_acceptable
+        end    
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end  
 end
