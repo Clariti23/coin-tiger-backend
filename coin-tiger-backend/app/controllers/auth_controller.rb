54c125b5378867aa04d585388f2667a74a98ab85
Farmwell <g.ferowich@gmail.com>
2020-02-07 13:58:40 -0500

    retested adding new favorites

--- coin-tiger-backend/app/controllers/auth_controller.rb
+++ coin-tiger-backend/app/controllers/auth_controller.rb
@@ -3,8 +3,9 @@ class AuthController < ApplicationController
  
     def create
       @user = User.find_by(email: user_login_params[:email])
-      
+      puts "--found user-----"
       if @user && @user.authenticate(user_login_params[:password])
+        puts "found---password-----"
         token = encode_token({ user_id: @user.id })
         render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
       else
--- coin-tiger-backend/app/controllers/baskets_controller.rb
+++ coin-tiger-backend/app/controllers/baskets_controller.rb
@@ -1,4 +1,5 @@
 class BasketsController < ApplicationController
+    skip_before_action :authorized
     def index 
         baskets = Basket.all 
         render json: baskets 
